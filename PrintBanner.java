ce5f3b2da418b34d6fd9d70006ba9ee7f93a493f
ketan-srm <ks1469@srmist.edu.in>
2026-03-01 17:00:44 +0530

    UC2

---
+++ PrintBanner.java
@@ -0,0 +1,15 @@
+public class PrintBanner
+{
+    public static void main(String[] args)
+    {
+        System.out.println("   ***       ***    ******        *****"  );
+        System.out.println(" **   **   **   **  **    **    **"       );
+        System.out.println("**     ** **     ** **      ** **"        );
+        System.out.println("**     ** **     ** **    **    **"       );
+        System.out.println("**     ** **     ** ******        ***"    );
+        System.out.println("**     ** **     ** **               **"  );
+        System.out.println("**     ** **     ** **                **" );
+        System.out.println(" **   **   **   **  **               ** " );
+        System.out.println("   ***       ***    **          *****"    );
+    }
+}
